2f844c8804168d1525eef7ab68f8681ced0cb7cf
Shion <shionchill@Shions-MacBook-Pro.local>
2020-05-07 16:30:47 +0700

    Updates

---
+++ precache-manifest.39e048c009bd0935a1eb1102ede952bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "446204838dd2a89f4d0685f9d90b8080",
+    "url": "/emoji-search/index.html"
+  },
+  {
+    "revision": "7d1c489916697e9cab83",
+    "url": "/emoji-search/static/js/2.80aad2a7.chunk.js"
+  },
+  {
+    "revision": "acfe23d1346bf98b69376d07dcec8eba",
+    "url": "/emoji-search/static/js/2.80aad2a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdbb412b720cbb13b8a2",
+    "url": "/emoji-search/static/js/main.9154e27d.chunk.js"
+  },
+  {
+    "revision": "fad9a3c7d71c876ce14b",
+    "url": "/emoji-search/static/js/runtime-main.974235e9.js"
+  }
+]);
